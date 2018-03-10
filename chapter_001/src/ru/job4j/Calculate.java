0a2b80842d6a4216633d9d537698d2ce9fff1cae
Yury Fabisheuski <fabishevskij@gmail.com>
2018-03-10 10:13:53 +0100

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/ru/job4j/Calculate.java
+++ chapter_001/src/ru/job4j/Calculate.java
@@ -1,7 +1,17 @@
 package ru.job4j;
-
+/**
+ * Calculate.
+ *
+ * @author Yury Fabisheuski (fabishevskij@gmail.com)
+ * @version $Id$
+ * @since 0.1
+*/
 public class Calculate{
-public static void main(String[] args){
-System.out.println("Hello World");
-}	
+	/**
+     * Main.
+     * @param args - args.
+     */
+	public static void main(String[] args){
+		System.out.println("Hello World");
+	}	
 }
