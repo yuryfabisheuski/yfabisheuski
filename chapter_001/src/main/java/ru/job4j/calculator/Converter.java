a4a7993891af60f0e896cbfa5e4d66ca019a8d10
Yury Fabisheuski <fabishevskij@gmail.com>
2018-03-19 15:05:08 +0100

    3.2. Конвертер валюты. [#41583]

--- chapter_001/src/main/java/ru/job4j/calculator/Converter.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Converter.java
@@ -4,52 +4,52 @@ package ru.job4j.calculator;
  * Корвертор валюты.
  */
 public class Converter {
+
+
     /**
      * Конвертируем рубли в евро.
      *
      * @param value рубли.
      * @return Евро.
      */
+    final static int CURSEURO = 70;
+    final static int CURSUSD = 60;
     public double rubleToEuro(int value) {
-        int curs = 70;
-        double euro = (double)value / curs;
-        return euro;
-    }
+
+        return (double)value / CURSEURO;}
 
     /**
      * Конвертируем рубли в доллары.
      *
-     * @param value рубли.
+     * @param value1 рубли.
      * @return Доллоры
      */
-    public double rubleToDollar(int value) {
-        int curs = 60;
-        double dollar = (double)value / curs;
-        return dollar;
+    public double rubleToDollar(int value1) {
+
+        return (double)value1 / CURSUSD;
     }
 
     /**
      * Конвертируем евро в рубли.
      *
-     * @param value евро.
+     * @param value2 евро.
      * @return Рубли.
      */
-    public double euroToRuble(int value) {
-        int curs = 70;
-        double ruble =(double)value * curs;
-        return ruble;
+    public double euroToRuble(int value2) {
+        return (double)value2 * CURSEURO;
+
     }
 
     /**
      * Конвертируем доллары в рубли.
      *
-     * @param value доллары.
+     * @param value3 доллары.
      * @return Рубли
      */
-    public double dollarToRuble(int value) {
-        int curs = 60;
-        double ruble =(double)value * curs;
-        return ruble;
+    public double dollarToRuble(int value3) {
+
+        return (double)value3 * CURSUSD ;
+
 
     }
 }
