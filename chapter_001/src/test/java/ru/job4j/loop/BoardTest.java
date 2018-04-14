62ab8a7e7c6221ed2e451600b813220413e69a2d
Yury Fabisheuski <fabishevskij@gmail.com>
2018-04-14 19:51:29 +0200

    4.3. Построить шахматную доску в п [13559]

--- chapter_001/src/test/java/ru/job4j/loop/BoardTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/BoardTest.java
@@ -17,6 +17,10 @@ public class BoardTest {
 
     @Test
     public void whenPaintBoardWithWidthFiveAndHeightFourThenStringWithFiveColsAndFourRows() {
-        //напишите здесь тест, проверяющий формирование доски 5 на 4.
+        Board board = new Board();
+        String result = board.paint(5, 4);
+        final String line = System.getProperty("line.separator");
+        String expected = String.format("X X X%s X X %sX X X%s X X %s", line, line, line, line);
+        assertThat(result, is(expected));  //напишите здесь тест, проверяющий формирование доски 5 на 4.
     }
 }
